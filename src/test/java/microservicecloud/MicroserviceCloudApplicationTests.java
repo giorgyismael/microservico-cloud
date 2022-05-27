c8148daa3418d22ead1c79f0a3274a28e9d0df5a
Giorgy Oliveira <giorgy.oliveira@neogrid.com>
2022-05-26 23:34:51 -0300

    subindo template microservice cloud

---
+++ src/test/java/microservicecloud/MicroserviceCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
