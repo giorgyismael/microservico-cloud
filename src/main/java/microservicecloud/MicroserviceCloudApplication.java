d3aafc8b1fccf551197657ad766ff9a8a6374896
Giorgy Oliveira <giorgy.oliveira@neogrid.com>
2022-05-29 02:01:25 -0300

    retirado portas do application

--- src/main/java/microservicecloud/MicroserviceCloudApplication.java
+++ src/main/java/microservicecloud/MicroserviceCloudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer //Habilita o server para ser um propagador de configurações
 public class MicroserviceCloudApplication {
 
 	public static void main(String[] args) {
